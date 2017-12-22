4fe03912c6d38da63f21215a057e19529e2964a6
Steve Lau <liuwenhong_steve@163.com>
2017-12-22 17:07:51 +0800

    Create myAllocator.cpp

---
+++ SGI-STL Test/allocator_test/myAllocator/myAllocator.cpp
@@ -0,0 +1,19 @@
+#include "myAllocator.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// allocator test
+int main()
+{
+	int arr[5] = {0, 1, 2, 3, 4};
+
+	vector<int, myAllocator::allocator<int> > iv(arr, arr + 5);
+
+	for (size_t i = 0; i < iv.size(); i++) {
+		cout << iv[i] << ' ';	
+	}
+
+	cout << endl;
+}
