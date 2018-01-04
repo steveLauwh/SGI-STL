81cd1427cbb498398e5fa4cf06d9e63af7536246
Steve Lau <liuwenhong_steve@163.com>
2018-01-04 11:40:58 +0800

    heap 操作函数实现

--- The Annotated STL Sources V3.3/container/sequence container/heap/stl_heap.h
+++ The Annotated STL Sources V3.3/container/sequence container/heap/stl_heap.h
@@ -26,7 +26,7 @@
 /* NOTE: This is an internal header file, included by other STL headers.
  *   You should not attempt to use it directly.
  */
-
+// heap 并没提供外部调用，在 SGI STL 里用于 priority queue 实现
 #ifndef __SGI_STL_INTERNAL_HEAP_H
 #define __SGI_STL_INTERNAL_HEAP_H
 
@@ -37,30 +37,32 @@ __STL_BEGIN_NAMESPACE
 #endif
 
 // Heap-manipulation functions: push_heap, pop_heap, make_heap, sort_heap.
-
+// push_heap 做 shift up 操作，调整满足 max-heap 特性
 template <class _RandomAccessIterator, class _Distance, class _Tp>
 void 
 __push_heap(_RandomAccessIterator __first,
             _Distance __holeIndex, _Distance __topIndex, _Tp __value)
 {
-  _Distance __parent = (__holeIndex - 1) / 2;
-  while (__holeIndex > __topIndex && *(__first + __parent) < __value) {
-    *(__first + __holeIndex) = *(__first + __parent);
-    __holeIndex = __parent;
-    __parent = (__holeIndex - 1) / 2;
+  _Distance __parent = (__holeIndex - 1) / 2; // 找到插入节点的父节点位置
+  while (__holeIndex > __topIndex && *(__first + __parent) < __value) { // 当插入值大于其父节点的值
+    *(__first + __holeIndex) = *(__first + __parent);   // 将其插入节点的父节点的值赋给插入节点位置
+    __holeIndex = __parent;  // 索引位置改变，插入值的索引为父节点索引
+    __parent = (__holeIndex - 1) / 2; // 插入值的索引的父节点
   }    
-  *(__first + __holeIndex) = __value;
+  *(__first + __holeIndex) = __value; // 交换完后，找到插入值的真正位置，赋值
 }
 
+// push_heap 操作的调整函数
 template <class _RandomAccessIterator, class _Distance, class _Tp>
 inline void 
 __push_heap_aux(_RandomAccessIterator __first,
                 _RandomAccessIterator __last, _Distance*, _Tp*)
 {
   __push_heap(__first, _Distance((__last - __first) - 1), _Distance(0), 
-              _Tp(*(__last - 1)));
+              _Tp(*(__last - 1)));  // 第三个参数就是要插入的值，位于 vector 尾部
 }
 
+// push_heap 实现，接受两个迭代器
 template <class _RandomAccessIterator>
 inline void 
 push_heap(_RandomAccessIterator __first, _RandomAccessIterator __last)
@@ -68,6 +70,7 @@ push_heap(_RandomAccessIterator __first, _RandomAccessIterator __last)
   __STL_REQUIRES(_RandomAccessIterator, _Mutable_RandomAccessIterator);
   __STL_REQUIRES(typename iterator_traits<_RandomAccessIterator>::value_type,
                  _LessThanComparable);
+  // 此函数被调用时，新元素应置于底部容器的最尾端
   __push_heap_aux(__first, __last,
                   __DISTANCE_TYPE(__first), __VALUE_TYPE(__first));
 }
@@ -98,6 +101,7 @@ __push_heap_aux(_RandomAccessIterator __first,
               _Tp(*(__last - 1)), __comp);
 }
 
+// 这个 push_heap 允许指定 “大小比较标准”
 template <class _RandomAccessIterator, class _Compare>
 inline void 
 push_heap(_RandomAccessIterator __first, _RandomAccessIterator __last,
@@ -108,25 +112,27 @@ push_heap(_RandomAccessIterator __first, _RandomAccessIterator __last,
                   __DISTANCE_TYPE(__first), __VALUE_TYPE(__first));
 }
 
+// vector 头部索引为 0 开始，__value 为尾部元素
+// pop_heap 调整
 template <class _RandomAccessIterator, class _Distance, class _Tp>
 void 
 __adjust_heap(_RandomAccessIterator __first, _Distance __holeIndex,
               _Distance __len, _Tp __value)
 {
-  _Distance __topIndex = __holeIndex;
-  _Distance __secondChild = 2 * __holeIndex + 2;
+  _Distance __topIndex = __holeIndex;  // __holeIndex = 0，为 heap 的根节点
+  _Distance __secondChild = 2 * __holeIndex + 2; // 根节点的右节点的索引
   while (__secondChild < __len) {
-    if (*(__first + __secondChild) < *(__first + (__secondChild - 1)))
+    if (*(__first + __secondChild) < *(__first + (__secondChild - 1))) // 比较根节点的左右节点值
       __secondChild--;
-    *(__first + __holeIndex) = *(__first + __secondChild);
+    *(__first + __holeIndex) = *(__first + __secondChild);  // while 里做 shift down 操作
     __holeIndex = __secondChild;
     __secondChild = 2 * (__secondChild + 1);
   }
-  if (__secondChild == __len) {
+  if (__secondChild == __len) {  // 没有右子节点，只有左子节点
     *(__first + __holeIndex) = *(__first + (__secondChild - 1));
     __holeIndex = __secondChild - 1;
   }
-  __push_heap(__first, __holeIndex, __topIndex, __value);
+  __push_heap(__first, __holeIndex, __topIndex, __value);  // 找到真正的位置，插入
 }
 
 template <class _RandomAccessIterator, class _Tp, class _Distance>
@@ -134,8 +140,8 @@ inline void
 __pop_heap(_RandomAccessIterator __first, _RandomAccessIterator __last,
            _RandomAccessIterator __result, _Tp __value, _Distance*)
 {
-  *__result = *__first;
-  __adjust_heap(__first, _Distance(0), _Distance(__last - __first), __value);
+  *__result = *__first;  // 先保存 vector 头部元素，放到 vector 的尾端
+  __adjust_heap(__first, _Distance(0), _Distance(__last - __first), __value); // 再做 shift down 操作调整
 }
 
 template <class _RandomAccessIterator, class _Tp>
@@ -144,9 +150,10 @@ __pop_heap_aux(_RandomAccessIterator __first, _RandomAccessIterator __last,
                _Tp*)
 {
   __pop_heap(__first, __last - 1, __last - 1, 
-             _Tp(*(__last - 1)), __DISTANCE_TYPE(__first));
+             _Tp(*(__last - 1)), __DISTANCE_TYPE(__first)); // 调整 heap 后，变为[first, last-1)
 }
 
+// pop_heap
 template <class _RandomAccessIterator>
 inline void pop_heap(_RandomAccessIterator __first, 
                      _RandomAccessIterator __last)
@@ -154,7 +161,7 @@ inline void pop_heap(_RandomAccessIterator __first,
   __STL_REQUIRES(_RandomAccessIterator, _Mutable_RandomAccessIterator);
   __STL_REQUIRES(typename iterator_traits<_RandomAccessIterator>::value_type,
                  _LessThanComparable);
-  __pop_heap_aux(__first, __last, __VALUE_TYPE(__first));
+  __pop_heap_aux(__first, __last, __VALUE_TYPE(__first));   // pop_heap 调整函数，将 vector 头部元素放到 vector 的尾部
 }
 
 template <class _RandomAccessIterator, class _Distance,
@@ -200,6 +207,7 @@ __pop_heap_aux(_RandomAccessIterator __first,
              __DISTANCE_TYPE(__first));
 }
 
+// pop_heap 允许指定大小比较标准
 template <class _RandomAccessIterator, class _Compare>
 inline void 
 pop_heap(_RandomAccessIterator __first,
@@ -209,22 +217,24 @@ pop_heap(_RandomAccessIterator __first,
   __pop_heap_aux(__first, __last, __VALUE_TYPE(__first), __comp);
 }
 
+// [first, last) 调整为 heap
 template <class _RandomAccessIterator, class _Tp, class _Distance>
 void 
 __make_heap(_RandomAccessIterator __first,
             _RandomAccessIterator __last, _Tp*, _Distance*)
 {
-  if (__last - __first < 2) return;
-  _Distance __len = __last - __first;
-  _Distance __parent = (__len - 2)/2;
+  if (__last - __first < 2) return; // 如果长度为 0 或 1，不排列
+  _Distance __len = __last - __first; // 待排列的长度
+  _Distance __parent = (__len - 2)/2; // 父节点i和右子节点2i+2
     
-  while (true) {
+  while (true) { // 这里 __parent 就是待插入节点索引
     __adjust_heap(__first, __parent, __len, _Tp(*(__first + __parent)));
     if (__parent == 0) return;
     __parent--;
   }
 }
 
+// make_heap 不指定大小比较标准，将现有的数据转化一个 heap
 template <class _RandomAccessIterator>
 inline void 
 make_heap(_RandomAccessIterator __first, _RandomAccessIterator __last)
@@ -246,7 +256,7 @@ __make_heap(_RandomAccessIterator __first, _RandomAccessIterator __last,
   _Distance __len = __last - __first;
   _Distance __parent = (__len - 2)/2;
     
-  while (true) {
+  while (true) { 
     __adjust_heap(__first, __parent, __len, _Tp(*(__first + __parent)),
                   __comp);
     if (__parent == 0) return;
@@ -254,6 +264,7 @@ __make_heap(_RandomAccessIterator __first, _RandomAccessIterator __last,
   }
 }
 
+// make_heap 允许指定大小比较标准
 template <class _RandomAccessIterator, class _Compare>
 inline void 
 make_heap(_RandomAccessIterator __first, 
@@ -264,6 +275,7 @@ make_heap(_RandomAccessIterator __first,
               __VALUE_TYPE(__first), __DISTANCE_TYPE(__first));
 }
 
+// sort_heap 不指定大小比较标准
 template <class _RandomAccessIterator>
 void sort_heap(_RandomAccessIterator __first, _RandomAccessIterator __last)
 {
@@ -271,9 +283,10 @@ void sort_heap(_RandomAccessIterator __first, _RandomAccessIterator __last)
   __STL_REQUIRES(typename iterator_traits<_RandomAccessIterator>::value_type,
                  _LessThanComparable);
   while (__last - __first > 1)
-    pop_heap(__first, __last--);
+    pop_heap(__first, __last--); // 调用 pop_heap
 }
 
+// sort_heap 允许指定大小比较标准
 template <class _RandomAccessIterator, class _Compare>
 void 
 sort_heap(_RandomAccessIterator __first,
