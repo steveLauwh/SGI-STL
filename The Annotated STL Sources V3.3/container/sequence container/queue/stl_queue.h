8f415bfbf12c707bb48837085b658b3a70d43f87
Steve Lau <liuwenhong_steve@163.com>
2018-01-04 15:35:51 +0800

    Update stl_queue.h

--- The Annotated STL Sources V3.3/container/sequence container/queue/stl_queue.h
+++ The Annotated STL Sources V3.3/container/sequence container/queue/stl_queue.h
@@ -145,7 +145,7 @@ operator>=(const queue<_Tp, _Sequence>& __x, const queue<_Tp, _Sequence>& __y)
 #endif /* __STL_FUNCTION_TMPL_PARTIAL_ORDER */
 
 
-// 优先队列
+// 优先队列，默认底层容器是 vector，利用 max-heap 规则
 template <class _Tp, 
           class _Sequence __STL_DEPENDENT_DEFAULT_TMPL(vector<_Tp>),
           class _Compare
@@ -169,15 +169,15 @@ public:
   typedef typename _Sequence::reference       reference;
   typedef typename _Sequence::const_reference const_reference;
 protected:
-  _Sequence c;
-  _Compare comp;
+  _Sequence c;  // 底层容器
+  _Compare comp; // 元素大小比较标准
 public:
   priority_queue() : c() {}
   explicit priority_queue(const _Compare& __x) :  c(), comp(__x) {}
   priority_queue(const _Compare& __x, const _Sequence& __s) 
     : c(__s), comp(__x) 
-    { make_heap(c.begin(), c.end(), comp); }
-
+    { make_heap(c.begin(), c.end(), comp); } // 构造函数实现，将底层容器具有 heap 特性
+// priority_queue 有几种构造函数形式
 #ifdef __STL_MEMBER_TEMPLATES
   template <class _InputIterator>
   priority_queue(_InputIterator __first, _InputIterator __last) 
@@ -218,20 +218,20 @@ public:
 
   bool empty() const { return c.empty(); }
   size_type size() const { return c.size(); }
-  const_reference top() const { return c.front(); }
+  const_reference top() const { return c.front(); } // 获得头部元素
   void push(const value_type& __x) {
     __STL_TRY {
-      c.push_back(__x); 
-      push_heap(c.begin(), c.end(), comp);
+      c.push_back(__x);  // 先向底层容器尾部插入元素
+      push_heap(c.begin(), c.end(), comp); // 重排 heap
     }
     __STL_UNWIND(c.clear());
   }
   void pop() {
     __STL_TRY {
-      pop_heap(c.begin(), c.end(), comp);
-      c.pop_back();
+      pop_heap(c.begin(), c.end(), comp); // 先调用 pop_heap，将最大值放到底层容器的尾部
+      c.pop_back(); // 然后从底层容器弹出
     }
-    __STL_UNWIND(c.clear());
+    __STL_UNWIND(c.clear()); 
   }
 };
 
